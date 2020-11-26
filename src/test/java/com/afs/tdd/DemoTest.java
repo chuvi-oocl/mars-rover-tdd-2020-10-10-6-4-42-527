00c54459cc27a0bf9306b0f7cd58e1a18d875c25
VICTOR CHU (CLOUD-ISD-OOCLL/HKG) <victor.chu@oocl.com>
2020-11-26 17:14:36 +0800

    [MarsRover][Victor] test: Add (0,0,N),M test

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -2,9 +2,20 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class DemoTest {
     @Test
-    void write_your_first_test() {
+    void should_return_0_1_N_when_run_instructions_given_0_0_N_M(){
 
+        //given
+        String instructions = "M";
+        MarsRover marsRover = new MarsRover(0, 0, "N");
+        //when
+        marsRover.runInstructions(instructions);
+        //then
+        assertEquals(marsRover.getCoordinateX(), 0);
+        assertEquals(marsRover.getCoordinateY(), 1);
+        assertEquals(marsRover.getDirection(), "N");
     }
 }
---
+++ src/test/java/com/afs/tdd/MarsRover.java
@@ -0,0 +1,24 @@
+package com.afs.tdd;
+
+public class MarsRover {
+    private int coordinateX;
+    private int coordinateY;
+    private String direction;
+    public MarsRover(int coordinateX, int coordinateY, String direction) {
+    }
+
+    public void runInstructions(String instructions) {
+    }
+
+    public int getCoordinateX() {
+        return 0;
+    }
+
+    public int getCoordinateY() {
+        return 0;
+    }
+
+    public String getDirection() {
+        return null;
+    }
+}
