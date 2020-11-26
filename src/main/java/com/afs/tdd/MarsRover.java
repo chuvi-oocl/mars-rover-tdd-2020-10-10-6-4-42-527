bf105e1117c2deaecc472ca54a2041c4f924fe17
VICTOR CHU (CLOUD-ISD-OOCLL/HKG) <victor.chu@oocl.com>
2020-11-26 17:18:57 +0800

    [MarsRover][Victor] feat: Complete (0,0,N),M test

---
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -0,0 +1,41 @@
+package com.afs.tdd;
+
+public class MarsRover {
+    private int coordinateX;
+    private int coordinateY;
+    private String direction;
+
+    public MarsRover(int coordinateX, int coordinateY, String direction) {
+        this.coordinateX = coordinateX;
+        this.coordinateY = coordinateY;
+        this.direction = direction;
+    }
+
+    public void runInstructions(String instructions) {
+        instructions.chars().mapToObj(instruction -> (char) instruction).forEach(this::runInstruction);
+    }
+
+    private void runInstruction(char instruction) {
+        if (instruction == 'M') {
+            move();
+        }
+    }
+
+    private void move() {
+        if (direction.equals("N")) {
+            this.coordinateY++;
+        }
+    }
+
+    public int getCoordinateX() {
+        return this.coordinateX;
+    }
+
+    public int getCoordinateY() {
+        return this.coordinateY;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+}
