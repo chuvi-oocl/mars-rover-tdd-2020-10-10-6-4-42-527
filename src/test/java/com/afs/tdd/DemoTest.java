8e4a2ab139a740e547a30028b168273951b6d191
VICTOR CHU (CLOUD-ISD-OOCLL/HKG) <victor.chu@oocl.com>
2020-11-26 17:21:15 +0800

    [MarsRover][Victor] test: Add (0,0,E),M test

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -32,4 +32,18 @@ class DemoTest {
         assertEquals(marsRover.getCoordinateY(), -1);
         assertEquals(marsRover.getDirection(), "S");
     }
+
+    @Test
+    void should_return_0_1_N_when_run_instructions_given_0_0_E_M() {
+
+        //given
+        String instructions = "M";
+        MarsRover marsRover = new MarsRover(0, 0, "E");
+        //when
+        marsRover.runInstructions(instructions);
+        //then
+        assertEquals(marsRover.getCoordinateX(), 1);
+        assertEquals(marsRover.getCoordinateY(), 0);
+        assertEquals(marsRover.getDirection(), "E");
+    }
 }
