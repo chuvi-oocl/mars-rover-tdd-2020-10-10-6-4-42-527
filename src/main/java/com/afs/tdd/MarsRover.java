e94c68f402eb63f1b3ad64fa164c1f78333a89c7
VICTOR CHU (CLOUD-ISD-OOCLL/HKG) <victor.chu@oocl.com>
2020-11-26 17:30:21 +0800

    [MarsRover][Victor] feat: Complete (0,0,W),M test

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -32,6 +32,9 @@ public class MarsRover {
             case "E":
                 this.coordinateX++;
                 break;
+            case "W":
+                this.coordinateX--;
+                break;
         }
     }
 
