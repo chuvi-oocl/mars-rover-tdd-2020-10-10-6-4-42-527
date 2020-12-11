676763b666b27159894d1612f02abdd5840a6589
VICTOR CHU (CLOUD-ISD-OOCLL/HKG) <victor.chu@oocl.com>
2020-12-11 19:37:34 +0800

    Add travis yml

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -196,4 +196,54 @@ class DemoTest {
         assertEquals(marsRover.getCoordinateY(), 1);
         assertEquals(marsRover.getDirection(), "E");
     }
+    
+    @Test
+    void should_parking_boy_call_parking_lot_park_function_once_when_park_the_car_given_call_parking_boy_park_function(){
+    
+        //given
+        ParkingLot parkingLot = Mockito.mock(ParkingLot.class);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        Car car = new Car();
+        //when
+        parkingBoy.park(car);
+        //then
+        verify(parkingLot, times(1)).park(car);
+    }
+    @Test
+    void should_return_a_parking_ticket_when_park_the_car_given_a_car_and_parking_lot(){
+        //given
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot();
+        //when
+        final Ticket ticket = parkingLot.park(car);
+        //then
+        assertNotNull(ticket);
+    }
+    @Test
+    void should_only_one_car_parked_when_park_multiple_cars_given_parking_lot_with_1_capacity(){
+        //given
+        Car car1 = new Car();
+        Car car2 = new Car();
+        ParkingLot parkingLot = new ParkingLot();
+        //when
+        final Ticket ticket1 = parkingLot.park(car1);
+        final Ticket ticket2 = parkingLot.park(car2);
+        //then
+        assertNotNull(ticket1);
+        assertNull(ticket2);
+    }
+    @Test
+    void should_return_car_when_fetch_car_given_parking_ticket_parking_lot_that_parked_the_car(){
+        //given
+        Car car1 = new Car();
+        Car car2 = new Car();
+        ParkingLot parkingLot = new ParkingLot();
+        //when
+        final Ticket ticket1 = parkingLot.park(car1);
+        final Ticket ticket2 = parkingLot.park(car2);
+        //then
+        assertNotNull(ticket1);
+        assertNull(ticket2);
+    }
+
 }
